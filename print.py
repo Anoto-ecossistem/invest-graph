0d3d75c2a8796de27977ef342c9eeba1f5c2ab3b
Matheus Alves <matheusalvessilvabr@gmail.com>
2024-05-12 17:24:57 -0300

    so much data from api alpha

--- print.py
+++ print.py
@@ -1,4 +1,12 @@
 from flask import Flask
+import requests
+
+url = 'https://www.alphavantage.co/query?function=MARKET_STATUS&apikey=YAKKXBYH26HZ14CD'
+r = requests.get(url)
+data = r.json()
+
+print(data)
+
 
 app = Flask(__name__)
 
